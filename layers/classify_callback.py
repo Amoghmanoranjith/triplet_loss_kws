298199271e0dddbf636a339c5a629cc02a167aff
Amogh <130673768+Amoghmanoranjith@users.noreply.github.com>
2025-01-03 17:56:26 +0530

    Update classify_callback.py
    yup im removing both of them

--- layers/classify_callback.py
+++ layers/classify_callback.py
@@ -6,8 +6,6 @@ from nemo.core.callbacks import ActionCallback
 import tensorflow as tf
 import tensorboard as tb
 
-tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
-
 
 class RunClassifierCallback(ActionCallback):
 
