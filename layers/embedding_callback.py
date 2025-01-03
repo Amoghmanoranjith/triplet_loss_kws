e1aae52c4c07e4a34696f72bc81ce8546453bf61
Amogh <130673768+Amoghmanoranjith@users.noreply.github.com>
2025-01-03 17:55:42 +0530

    Update embedding_callback.py
    i give up ill just remove the part giving me error

--- layers/embedding_callback.py
+++ layers/embedding_callback.py
@@ -15,9 +15,6 @@ from nemo.core.callbacks import ActionCallback
 import tensorflow as tf
 import tensorboard as tb
 
-tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
-
-
 class EmbeddingEvaluatorCallback(ActionCallback):
     """
     For callback documentation: please see
